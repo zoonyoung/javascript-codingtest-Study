1775919689d451b3cd8e675aaed87582ae107f0b
Junyoung Park <9851248@gmail.com>
2024-02-13 16:28:01 +0900

    zoonyoung: 82612 완료

---
+++ Level1/82612.js
@@ -0,0 +1,7 @@
+function solution(price, money, count) {
+  let needMoney = 0;
+  for (let i = 1; i <= count; i++) {
+    needMoney += price * i;
+  }
+  return needMoney - money <= 0 ? 0 : needMoney - money;
+}
